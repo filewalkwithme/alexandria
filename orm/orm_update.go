c0ed40f3da54654972c5870d3dab120e0c22e7f5
maiconio <maiconscosta@gmail.com>
2016-06-03 22:54:55 -0300

    Create update method

--- orm/orm_update.go
+++ orm/orm_update.go
@@ -1,7 +1,7 @@
 package orm
 
 import (
-	//	"fmt"
+	"fmt"
 	"reflect"
 	"strconv"
 )
@@ -30,13 +30,35 @@ func (handler *Handler) assembleSQLUpdateStatement() {
 	fieldMap = append(fieldMap, saveField{name: "ID", fieldType: "int"})
 
 	sqlInstruction = sqlInstruction[:len(sqlInstruction)-2]
-	sqlInstruction = sqlInstruction + " where id = ?;"
+	sqlInstruction = sqlInstruction + " where id = $" + strconv.Itoa(j) + ";"
 
 	handler.sqlUpdate = sqlInstruction
 	handler.mapUpdate = fieldMap
 }
 
 func (handler Handler) update(objectPtr interface{}) error {
+	object := reflect.ValueOf(objectPtr).Elem()
+	tableName := reflect.TypeOf(objectPtr).Elem().Name()
+	if tableName != handler.tableName {
+		return fmt.Errorf("Object table name (%v) is diferent from handler table name (%v)", tableName, handler.tableName)
+	}
+
+	//build the arguments array
+	var args []interface{}
+	for _, field := range handler.mapUpdate {
+		if field.fieldType == "int" {
+			args = append(args, int(object.FieldByName(field.name).Int()))
+		}
+		if field.fieldType == "string" {
+			args = append(args, string(object.FieldByName(field.name).String()))
+		}
+	}
+
+	//run Update
+	_, err := handler.db.Exec(handler.sqlUpdate, args...)
+	if err != nil {
+		return err
+	}
 
 	return nil
 }
